27629c545b00f8023a8d4b7929a26fab5924e32e
Tuychiev Otabek <otabekotabek996996@gmail.com>
2023-10-29 21:58:48 +0500

    Solved the task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketsArray = bracketsConfig.map(item => item.join(''));
+
+  let strLength = str.length;
+  let oldStrLength = 0;
+
+  while (strLength !== oldStrLength) {
+    bracketsArray.forEach(item => str = str.replace(item, ''));
+    oldStrLength = strLength;
+    strLength = str.length;
+  }
+
+  return str.length === 0;
 }
